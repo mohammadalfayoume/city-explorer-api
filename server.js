5d6bed4a22193f0e7ad2c16e285e87d0912b487f
Mohammad Alfayoume <mabed3819@gmail.com>
2022-08-30 16:46:49 +0300

    Done

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express"); // import express framework
 const server = express(); //put every pre defiened methods of express inside server variable
-const wheatherData = require("./assets/weather.json");
+const weatherData = require("./assets/weather.json");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -15,57 +15,53 @@ server.use(cors()); //this allow any client send me a request or make the server
 
 const PORT = process.env.PORT;
 
-// http://localhost:3000/
+// http://localhost:3001/
 server.get("/", (req, res) => {
   res.send("Hi from the home route");
 });
 
-// http://localhost:3000/wheather?lat=lat&lon=lon&city=city
-// server.get("/wheather", (req, res) => {
-//   let selectedCity = wheatherData.find((city) => {
-//     console.log(
-//       `${city.lat} == ${req.query.lat} && ${city.lon} == ${req.query.lon} && ${city.city_name} == ${req.query.city}`
-//     );
-
-//     if (city.city_name == req.query.city_name) {
-//       return city;
-//     } else if (
-//       parseInt(city.lat) == parseInt(req.query.lat) &&
-//       parseInt(city.lon) == parseInt(req.query.lon)
-//     ) {
-//       return city;
-//     } else {
-//       return "not found";
-//     }
-//   });
-//   res.send(selectedCity);
-// });
-
+// http://localhost:3002/wheather?lat=lat&lon=lon&city=city
 server.get("/wheather", (req, res) => {
-  let selectedCityName = wheatherData.find((item) => {
-    console.log(`${item.city_name} == ${req.query.city}`);
+  let selectedCity = weatherData.find((item) => {
+    console.log(
+      `${item.lat} == ${req.query.lat} && ${item.lon} == ${req.query.lon} && ${item.city_name} == ${req.query.city}`
+    );
+    if (
+      item.city_name == "" ||
+      item.city_name == undefined ||
+      item.lat == undefined ||
+      item.lon == undefined
+    ) {
+      let err = {
+        error: "Error",
+        msg: "try again",
+      };
+
+      return err
+    }
+
     if (item.city_name == req.query.city) {
       return item;
     }
-    else {
-        return 'not found'
-      }
+    if (
+      parseInt(item.lat) == parseInt(req.query.lat) &&
+      parseInt(item.lon) == parseInt(req.query.lon)
+    ) {
+      return item;
+    }
   });
-  res.send(selectedCityName);
-});
 
-server.get("/wheather", (req, res) => {
-    let selectedLatAndLon = wheatherData.find((item) => {
-      console.log(`${item.lat} == ${req.query.lat} && ${item.lon} == ${req.query.lon}`);
-      if (item.lat == req.query.lat && item.lon == req.query.lon) {
-        return item;
-      }
-      else {
-        return 'not found'
-      }
-    });
-    res.send(selectedLatAndLon);
+  let returnData = selectedCity.data.map((day) => {
+    let specificFormatObj = {
+      description: `low of ${day.low_temp},  high of ${day.max_temp} with ${day.weather.description}`,
+      date: day.valid_date,
+    };
+    return specificFormatObj;
   });
+
+  console.log(returnData);
+  res.send(returnData);
+});
 
 server.get("*", (req, res) => {
   res.send("Page not found");
